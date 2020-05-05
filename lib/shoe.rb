deded5e5d7ba21ee35d4b6b923d1435ca892b412
Matt Im <matthew.seiim@gmail.com>
2020-05-05 21:27:19 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,11 +1,15 @@
 # Make your shoe class here!
 
 class Brand
-  attr_accessor :color, :size, :material, :conditioon
+  attr_accessor :color, :size, :material, :condition
   attr_reader :brand
   
   def initialize(brand)
     @brand = brand
   end
   
+  def cobble
+    puts "show has been repaired"
+    @condition = new 
+  end
 end
